76048860897c076dc85fa53e36a3b90b86b3a533
Fernanda Trujillo <77870297+Ferain93@users.noreply.github.com>
2026-04-07 10:57:44 -0500

    feat: created initial tables (#2)

---
+++ database/migrations/20260406151741_create_users_table.js
@@ -0,0 +1,15 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.string('email', 255).notNullable().unique();
+    table.string('password_hash', 255).notNullable();
+    table.integer('role_id').unsigned().notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+
+    table.foreign('role_id').references('id').inTable('roles').onDelete('RESTRICT');
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('users');
+};
